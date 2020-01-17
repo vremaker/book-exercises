e177a5d558ee73f087c75751fb4985134750bd95
Valerie <vremaker@gmail.com>
2020-01-16 18:49:14 -0800

    week 1 exercises

--- chapter-07-exercises/exercise-2/exercise.R
+++ chapter-07-exercises/exercise-2/exercise.R
@@ -2,22 +2,22 @@
 
 # Create a vector `first_ten` that has the values 10 through 20 in it (using 
 # the : operator)
-
+first_ten <- c(10:20)
 
 # Create a vector `next_ten` that has the values 21 through 30 in it (using the 
 # seq() function)
-
+next_ten <- c(21:30)
 
 # Create a vector `all_numbers` by combining the previous two vectors
-
+all_numbers <-c(first_ten, next_ten)
 
 # Create a variable `eleventh` that contains the 11th element in `all_numbers`
-
+eleventh <- all_numbers[11];
 
 # Create a vector `some_numbers` that contains the 2nd through the 5th elements 
 # of `all_numbers`
 
-
+some_numbers <- all_numbers[2:11]
 
 # Create a vector `even` that holds the even numbers from 1 to 100
 
@@ -26,16 +26,15 @@
 # numbers in your `even` vector are even
 
 
-
 # Create a vector `phone_numbers` that contains the numbers 8, 6, 7, 5, 3, 0, 9
-
+phone_number <-c(8,6,7,5,3,0,9) # I'M CRYING I LOVE THIS 
 
 # Create a vector `prefix` that has the first three elements of `phone_numbers`
+prefix <- phone_number[1:3]
 
 
 # Create a vector `small` that has the values of `phone_numbers` that are 
 # less than or equal to 5
-
 
 # Create a vector `large` that has the values of `phone_numbers` that are 
 # strictly greater than 5
