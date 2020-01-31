5394e5d7b02e6e6e9a14d24090b69d81e8101038
Valerie <vremaker@gmail.com>
2020-01-30 16:09:37 -0800

    woo

--- chapter-11-exercises/exercise-1/exercise.R
+++ chapter-11-exercises/exercise-1/exercise.R
@@ -11,19 +11,20 @@ library(fueleconomy)
 
 # You should now have access to the `vehicles` data frame
 # You can use `View()` to inspect it
-
+View(vehicles)
 
 # Select the different manufacturers (makes) of the cars in this data set. 
 # Save this vector in a variable
-
-
+car_makes <- c(vehicles$make)
+print(car_makes)
 # Use the `unique()` function to determine how many different car manufacturers
 # are represented by the data set
-
-
+car_makes_uniq <- c(unique(vehicles$make))
+print(car_makes_uniq)
 # Filter the data set for vehicles manufactured in 1997
-
-
+cars_1997 <- vehicles[vehicles$year == 1997,]
+print(cars_1997)
+View(cars_1997)
 # Arrange the 1997 cars by highway (`hwy`) gas milage
 # Hint: use the `order()` function to get a vector of indices in order by value
 # See also:
@@ -32,17 +33,20 @@ library(fueleconomy)
 
 # Mutate the 1997 cars data frame to add a column `average` that has the average
 # gas milage (between city and highway mpg) for each car
-
-
+cars_1997$average <- (cars_1997$hwy + cars_1997$cty )/2
+View(cars_1997)
 # Filter the whole vehicles data set for 2-Wheel Drive vehicles that get more
 # than 20 miles/gallon in the city. 
 # Save this new data frame in a variable.
-
+vehicles_2wd <- vehicles[vehicles$drive == "2-Wheel Drive",]
+efficient_2wd <- vehicles_2wd[vehicles_2wd$city > 20,]
 
 # Of the above vehicles, what is the vehicle ID of the vehicle with the worst 
 # hwy mpg?
 # Hint: filter for the worst vehicle, then select its ID.
-
+vehicles_2wd[vehicles_2wd$hwy == min(vehicles_2wd$hwy), "id"]
+vehicles_2wd$id
+vehicles_2wd$hwy
 
 # Write a function that takes a `year_choice` and a `make_choice` as parameters, 
 # and returns the vehicle model that gets the most hwy miles/gallon of vehicles 
@@ -51,4 +55,4 @@ library(fueleconomy)
 
 
 # What was the most efficient Honda model of 1995?
-
+install.packages("dplyr")
--- chapter-11-exercises/exercise-4/exercise.R
+++ chapter-11-exercises/exercise-4/exercise.R
@@ -3,22 +3,26 @@
 # Install the `"nycflights13"` package. Load (`library()`) the package.
 # You'll also need to load `dplyr`
 
-
+library("dplyr")
+install.packages("dplyr")
+library("nycflights13")
 # The data frame `flights` should now be accessible to you.
 # Use functions to inspect it: how many rows and columns does it have?
 # What are the names of the columns?
 # Use `??flights` to search for documentation on the data set (for what the 
 # columns represent)
+flights <-library("nycflights13")
 
-
+View(flights)
 # Use `dplyr` to give the data frame a new column that is the amount of time
 # gained or lost while flying (that is: how much of the delay arriving occured
 # during flight, as opposed to before departing).
+flights <-mutate(flights, delayed_in_air = arr_delay - dep_delay )
 
 
 # Use `dplyr` to sort your data frame in descending order by the column you just
 # created. Remember to save this as a variable (or in the same one!)
-
+flights <- arrannge(flights, -delay_in_air)
 
 # For practice, repeat the last 2 steps in a single statement using the pipe
 # operator. You can clear your environmental variables to "reset" the data frame
@@ -29,16 +33,21 @@
 
 # On average, did flights gain or lose time?
 # Note: use the `na.rm = TRUE` argument to remove NA values from your aggregation
-
+summarize(flights, avg = mean(delayed_inn_air, na.rm = TRUE))
 
 # Create a data.frame of flights headed to SeaTac ('SEA'), only including the
 # origin, destination, and the "gain_in_air" column you just created
 
+to_sea <- filter(flights, dest == "SEA")
 
 # On average, did flights to SeaTac gain or loose time?
-
+summarize(to_sea, avg_delated = mean(delayed_in_air, na.rm = TRUE))
 
 # Consider flights from JFK to SEA. What was the average, min, and max air time
 # of those flights? Bonus: use pipes to answer this question in one statement
 # (without showing any other data)!
 
+flights %>%
+  filter(dest == "SEA") %>%
+  select(origin, dest, delayed_in_air) %>%
+  summarizd(av_delayed = mean(delayed_in_air, na.rm = TRUE))
